18cbfa6f4aaf9f4c55e1551d38e405e37e4507a9
Michael Copeland <michael.james.copeland@gmail.com>
2016-07-17 22:12:01 -0400

    Slight tinkering with some definitions of classes and API functions

--- aiodownload/api.py
+++ aiodownload/api.py
@@ -4,29 +4,32 @@ from aiodownload import AioDownloadBundle, AioDownload
 import asyncio
 
 
-def one(url, loop=None):
+def one(url, download=None):
 
-    return [s for s in swarm([url], loop=loop)][0]
-
-
-def swarm(urls, loop=None):
-
-    return [e for e in each(urls, loop=loop)]
+    return [s for s in swarm([url], download=download)][0]
 
 
-def each(iterable, url_map=None, loop=None):
+def swarm(urls, download=None):
+
+    return [e for e in each(urls, download=download)]
+
+
+def each(iterable, url_map=None, download=None):
 
     url_map = url_map or _url_map
+    download = download or AioDownload()
 
-    event_loop = loop or asyncio.get_event_loop()
+    tasks = []
+    for i in iterable:
+        url = url_map(i)
+        info = None if i == url else i
+        tasks.append(
+            download._loop.create_task(
+                AioDownload(url, info=info)
+            )
+        )
 
-    download = AioDownload(event_loop)
-    tasks = [
-        event_loop.create_task(
-            download.main(AioDownloadBundle(url_map(i), info=i))
-        ) for i in iterable
-    ]
-    for task_set in event_loop.run_until_complete(asyncio.wait(tasks)):
+    for task_set in download._loop.run_until_complete(asyncio.wait(tasks)):
         for task in task_set:
             yield task.result()
 
