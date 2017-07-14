dcbaaf11cfeffad7936352af9194b0dff9cb4048
Michael Copeland <michael.james.copeland@gmail.com>
2017-07-13 20:53:57 -0400

    Bump up version

--- setup.py
+++ setup.py
@@ -27,9 +27,9 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.4',
+    version='0.2.5',
 
-    description='Asychronous downloads made easy',
+    description='Asynchronous Requests and Downloads Without Thinking About It',
     long_description=long_description,
 
     # The project's main homepage.
